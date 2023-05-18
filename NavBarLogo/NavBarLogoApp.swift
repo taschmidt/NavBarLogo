8770fdba9be362af6f621e2bc4652c889c346d47
Tim Schmidt <tim.schmidt@dowjones.com>
2023-05-18 15:13:26 -0500

    Initial Commit

---
+++ NavBarLogo/NavBarLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavBarLogoApp.swift
+//  NavBarLogo
+//
+//  Created by schmidtt on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct NavBarLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
